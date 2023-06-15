518bf87a38fe2ac105049a59b10bb603c39dd187
Joshua Nicholson <94021017+jnicholCU@users.noreply.github.com>
2023-06-15 13:26:26 -0600

    Content Frame Update
    Added option for frame color to the background section of layout builder.
    
    Closes #12

--- src/UCBLayout.php
+++ src/UCBLayout.php
@@ -70,4 +70,10 @@ final class UCBLayout {
   public const ROW_BACKGROUND_EFFECT_SCROLL = 'fixed';
 
   public const ROW_BACKGROUND_EFFECT_PARALLAX = 'parallax';
+
+  public const ROW_CONTENT_FRAME_COLOR_NONE = 'white';
+
+  public const ROW_CONTENT_FRAME_COLOR_LIGHT_GRAY = 'light-gray';
+
+  public const ROW_CONTENT_FRAME_COLOR_DARK_GRAY = 'dark-gray';
 }
